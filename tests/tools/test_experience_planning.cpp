780439c435f522c5a8711c299c54ccd23a64e757
HiroIshida <spitfire.docomo@gmail.com>
2023-01-20 20:35:38 +0900

    test: check if using with database is better than without

--- tests/tools/test_experience_planning.cpp
+++ tests/tools/test_experience_planning.cpp
@@ -34,6 +34,8 @@
 
 /* Author: Hirokazu Ishida */
 
+#include <type_traits>
+#include "ompl/util/Console.h"
 #define BOOST_TEST_MODULE "ExperienceBasedPlanning"
 #include <boost/test/unit_test.hpp>
 #include <boost/filesystem.hpp>
@@ -87,11 +89,18 @@ struct ExperienceBasedPlannerTest
         space->addDimension(0.0, 1.0);
         space->addDimension(0.0, 1.0);
         const auto si = std::make_shared<ob::SpaceInformation>(space);
+        si->setStateValidityCheckingResolution(0.002);
         auto exp_planner = std::make_unique<ExpPlannerT>(si);
         PlannerSetter<ExpPlannerT, PlannerT, ReplannerT>::setPlanners(exp_planner);
         exp_planner->setStateValidityChecker([](const ob::State *state) {
-            (void)state;
-            return true;
+            const auto rs = state->as<ob::RealVectorStateSpace::StateType>();
+            double x = rs->values[0];
+            double y = rs->values[1];
+            if (x < 0.2 || x > 0.8)
+            {
+                return true;
+            }
+            return std::abs(y - 0.5) < 0.004;
         });
 
         return exp_planner;
@@ -100,7 +109,7 @@ struct ExperienceBasedPlannerTest
     static void runTest()
     {
         const auto temp_path = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path();
-        const size_t n_problem = 5;
+        const size_t n_problem = 30;
 
         // solve problem
         {
@@ -110,8 +119,9 @@ struct ExperienceBasedPlannerTest
             const auto valid_sampler = exp_planner->getSpaceInformation()->allocValidStateSampler();
             // note that the experience is not always stored even when solved
             // so we need while loop
-            while (exp_planner->getExperiencesCount() < 5)
+            while (exp_planner->getExperiencesCount() < n_problem)
             {
+                exp_planner->clear();
                 ob::ScopedState<> start(exp_planner->getStateSpace());
                 valid_sampler->sample(start.get());
                 ob::ScopedState<> goal(exp_planner->getStateSpace());
@@ -133,9 +143,13 @@ struct ExperienceBasedPlannerTest
             const auto space = exp_planner->getSpaceInformation()->getStateSpace();
             const auto valid_sampler = exp_planner->getSpaceInformation()->allocValidStateSampler();
             ob::ScopedState<> start(exp_planner->getStateSpace());
-            valid_sampler->sample(start.get());
             ob::ScopedState<> goal(exp_planner->getStateSpace());
-            valid_sampler->sample(goal.get());
+            auto rs_start = start->as<ob::RealVectorStateSpace::StateType>();
+            auto rs_goal = goal->as<ob::RealVectorStateSpace::StateType>();
+            rs_start->values[0] = 0.1;
+            rs_start->values[1] = 0.1;
+            rs_goal->values[0] = 0.9;
+            rs_goal->values[1] = 0.9;
             exp_planner->setStartAndGoalStates(start, goal);
 
             BOOST_CHECK_EQUAL(exp_planner->getExperiencesCount(), 0);
@@ -143,10 +157,29 @@ struct ExperienceBasedPlannerTest
             exp_planner->setup();  // load experience
             BOOST_CHECK_EQUAL(exp_planner->getExperiencesCount(), n_problem);
 
+            OMPL_INFORM("solve without database");
+            exp_planner->clear();
+            exp_planner->enablePlanningFromScratch(true);
+            exp_planner->enablePlanningFromRecall(false);
+            const auto result_without_experience = exp_planner->solve(10.0);
+            BOOST_CHECK(result_without_experience);
+            const auto elapsed_without_db = exp_planner->getLastPlanComputationTime();
+
+            OMPL_INFORM("solve with database");
+            exp_planner->clear();
             exp_planner->enablePlanningFromScratch(false);
-            exp_planner->enablePlanningFromRecall(true);  // force to solve using experience
-            const auto result = exp_planner->solve(10.0);
-            BOOST_CHECK(result);
+            exp_planner->enablePlanningFromRecall(true);
+            const auto result_with_experience = exp_planner->solve(10.0);
+            const auto elapsed_with_db = exp_planner->getLastPlanComputationTime();
+            BOOST_CHECK(result_with_experience);
+
+            OMPL_INFORM("with db: %f, without db: %f", elapsed_with_db, elapsed_without_db);
+            if constexpr (std::is_same_v<PlannerT, ReplannerT>)
+            {
+                // solving with database must be faster than without database
+                // at least if planner and replanner types are the same
+                BOOST_CHECK_LT(elapsed_with_db, elapsed_without_db);
+            }
         }
     }
 };
